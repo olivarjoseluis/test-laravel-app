e30f58812683c8d07ea98b8da9d565ca704b441a
Jose Luis Olivar <olivarjoseluis9@gmail.com>
2023-03-16 22:19:48 -0500

    Upload file

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,11 @@ Route::get('/', function () {
 Route::get('about', function () {
   return 'About us';
 });
+
+Route::view('profile', 'profile');
+
+Route::post('profile', function (\Illuminate\Http\Request $request) {
+  $request->file('photo')->store('profiles');
+
+  return redirect('profile');
+});
